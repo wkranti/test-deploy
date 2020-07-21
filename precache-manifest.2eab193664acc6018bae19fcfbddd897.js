4a2d37ac6a0f237eadb0ca8c373e6a4c3137c1a5
unknown <wkranti8@gmail.com>
2020-07-21 19:13:47 +0530

    Updates

---
+++ precache-manifest.2eab193664acc6018bae19fcfbddd897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e876568924133beab5f7e656490a8bf",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "6a629854276346670d41",
+    "url": "/test-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f0670f47d9c056a3bfe",
+    "url": "/test-deploy/static/js/2.58444768.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy/static/js/2.58444768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a629854276346670d41",
+    "url": "/test-deploy/static/js/main.6241ac15.chunk.js"
+  },
+  {
+    "revision": "e8ac38734cbdc74e8fd0",
+    "url": "/test-deploy/static/js/runtime-main.8cb0ec19.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
